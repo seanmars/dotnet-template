a1cd545f9ab74312e4d0ef967e2567c7ed3029e1
seanmars <mars0222@gmail.com>
2024-02-07 00:02:53 +0800

    Add Coravel project

---
+++ CoravelSample/Program.cs
@@ -0,0 +1,38 @@
+﻿// https://docs.coravel.net/Scheduler/
+
+using Coravel;
+using Coravel.Invocable;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+var hostBuilder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddScheduler();
+        services.AddTransient<SampleInvocable>();
+    });
+
+var host = hostBuilder.Build();
+
+host.Services.UseScheduler(scheduler =>
+{
+    scheduler
+        .Schedule<SampleInvocable>()
+        .EveryFiveSeconds();
+
+    scheduler
+        .Schedule<SampleInvocable>()
+        .DailyAt(0, 15)
+        .Zoned(TimeZoneInfo.Local);
+});
+host.Run();
+
+class SampleInvocable : IInvocable
+{
+    public Task Invoke()
+    {
+        Console.WriteLine("Hello from Coravel!");
+        return Task.CompletedTask;
+    }
+}
