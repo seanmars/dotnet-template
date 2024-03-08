cd39c64b3d4df59100c0101bb5e8eed51b9b6300
seanmars <mars0222@gmail.com>
2024-03-08 18:09:46 +0800

    Add SqlWithJson project

---
+++ SqlWithJson/AppDbContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SqlWithJson;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).HasMaxLength(64).IsRequired();
+            entity.OwnsOne(e => e.Goods, b =>
+            {
+                b.ToJson();
+
+                b.OwnsMany(e => e.Items);
+                b.OwnsMany(e => e.Equipments);
+            });
+        });
+    }
+}
---
+++ SqlWithJson/Program.cs
@@ -0,0 +1,54 @@
+﻿using Bogus;
+using SqlWithJson;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddLogging(builder =>
+{
+    builder.AddConsole()
+        .SetMinimumLevel(LogLevel.Debug);
+});
+
+serviceCollection.AddDbContextPool<AppDbContext>(options =>
+{
+    options.UseSqlite("DataSource=./storage/sqlite-json.db;Cache=Shared");
+
+    options.EnableDetailedErrors();
+    options.EnableSensitiveDataLogging();
+});
+
+// Create user data by Bogus
+var userFaker = new Faker<User>()
+    .RuleFor(u => u.Name, f => f.Person.FullName)
+    .RuleFor(u => u.Goods, f =>
+    {
+        var range = Enumerable.Range(1, 100);
+        var randomized = f.Random.Shuffle(range).ToList();
+
+        return new Goods()
+        {
+            Items = f.Make(f.Random.Int(1, 30), () => new Item
+            {
+                ItemId = f.PickRandom(randomized),
+                Count = f.PickRandom(randomized)
+            }).ToList(),
+            Equipments = f.Make(f.Random.Int(1, 20), () => new Equipment
+            {
+                ItemId = f.PickRandom(randomized),
+                Str = f.PickRandom(randomized),
+                Dex = f.PickRandom(randomized),
+                Wis = f.PickRandom(randomized),
+            }).ToList()
+        };
+    });
+
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+await using var dbContext = serviceProvider.GetRequiredService<AppDbContext>();
+await dbContext.Database.EnsureDeletedAsync();
+await dbContext.Database.EnsureCreatedAsync();
+
+await dbContext.Users.AddRangeAsync(userFaker.Generate(100));
+await dbContext.SaveChangesAsync();
