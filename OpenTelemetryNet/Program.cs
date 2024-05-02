5b82791b6b42c6e51c43998345d5dffe548f556d
seanmars <mars0222@gmail.com>
2024-05-02 17:07:57 +0800

    Add OpenTelemetry project

---
+++ OpenTelemetryNet/Program.cs
@@ -0,0 +1,84 @@
+using System.Reflection;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var serviceName = Assembly.GetExecutingAssembly().GetName().Name ?? "OpenTelemetryNet";
+var serviceVersion = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+    ?.InformationalVersion ?? "1.0.0";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddOpenTelemetry()
+    .WithTracing(tracing =>
+    {
+        tracing
+            .AddSource(serviceName)
+            .ConfigureResource(resource =>
+                resource.AddService(
+                    serviceName: serviceName,
+                    serviceVersion: serviceVersion))
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation()
+            .AddConsoleExporter()
+            .AddOtlpExporter();
+    })
+    .WithMetrics(metrics =>
+    {
+        metrics
+            .AddRuntimeInstrumentation()
+            .AddMeter(
+                "Microsoft.AspNetCore.Hosting",
+                "Microsoft.AspNetCore.Server.Kestrel",
+                "Microsoft.AspNetCore.Routing"
+            )
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddConsoleExporter()
+            .AddOtlpExporter();
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ OpenTelemetryNet/README.md
@@ -0,0 +1,55 @@
+﻿# OpenTelemetry
+
+使用 OpenTelemetry 搭配 Jaeger 為 .NET WebApi 應用程式增加 Trace 與 Metrics 功能。
+
+## OpenTelemetry Packages
+
+```shell
+dotnet add package OpenTelemetry
+dotnet add package OpenTelemetry.Exporter.Jaeger
+dotnet add package OpenTelemetry.Exporter.Console
+dotnet add package OpenTelemetry.Exporter.OpenTelemetryProtocol
+dotnet add package OpenTelemetry.Instrumentation.AspNetCore
+dotnet add package OpenTelemetry.Instrumentation.Http
+dotnet add package OpenTelemetry.Instrumentation.EntityFrameworkCore
+```
+
+## Jaeger
+
+- [Jaeger](https://www.jaegertracing.io/)
+
+Run Jaeger in Docker With PowerShell:
+
+```powershell
+docker run -d --rm --name jaeger `
+  -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 `
+  -p 6831:6831/udp `
+  -p 6832:6832/udp `
+  -p 5778:5778 `
+  -p 16686:16686 `
+  -p 4317:4317 `
+  -p 4318:4318 `
+  -p 14250:14250 `
+  -p 14268:14268 `
+  -p 14269:14269 `
+  -p 9411:9411 `
+  jaegertracing/all-in-one:latest
+```
+
+Or Bash:
+
+```shell
+docker run -d --rm --name jaeger \
+  -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
+  -p 6831:6831/udp \
+  -p 6832:6832/udp \
+  -p 5778:5778 \
+  -p 16686:16686 \
+  -p 4317:4317 \
+  -p 4318:4318 \
+  -p 14250:14250 \
+  -p 14268:14268 \
+  -p 14269:14269 \
+  -p 9411:9411 \
+  jaegertracing/all-in-one:latest
+```
