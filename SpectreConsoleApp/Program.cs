3999c5abaaed9d77e2b88c38e41c9d45cb4b8782
seanmars <mars0222@gmail.com>
2024-06-05 18:00:11 +0800

    Add SpectreConsoleApp template

---
+++ SpectreConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Spectre.Console;
+
+await AnsiConsole.Progress()
+    .Columns([
+        new TaskDescriptionColumn(),
+        new ProgressBarColumn(),
+        new PercentageColumn(),
+        new SpinnerColumn(),
+    ])
+    .StartAsync(async ctx =>
+    {
+        // Define tasks
+        var task1 = ctx.AddTask("[green]Retriculating algorithms[/]");
+        var task2 = ctx.AddTask("[green]Colliding splines[/]");
+
+        while (!ctx.IsFinished)
+        {
+            await Task.Delay(50);
+            task1.Increment(1.5);
+            task2.Increment(2.8);
+        }
+    });
