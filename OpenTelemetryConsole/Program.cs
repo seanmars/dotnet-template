4267530228c01a0e8bc2b619d625eee34994d461
seanmars <mars0222@gmail.com>
2024-06-13 11:18:26 +0800

    Add open telemetry for console

---
+++ OpenTelemetryConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("my-service"))
+    .AddSource("my-service")
+    .AddConsoleExporter()
+    .Build();
+
+using var activitySource = new ActivitySource("my-service");
+using var span = activitySource.StartActivity("Main");
+await Task.Delay(1000);
+span?.AddEvent(new ActivityEvent("1"));
+
+await Task.Delay(2000);
+span?.AddEvent(new ActivityEvent("2"));
